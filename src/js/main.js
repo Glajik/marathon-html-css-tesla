2a246a79838492abefa05226ed90b21a9dbe52bd
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-03-13 12:30:33 +0200

    Отображение меню по событию нажатия кнопки, + анимация.

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,13 @@ $(function(){
     autoplay: true,
     autoplaySpeed: 30000,
     dots: true
-  })
+  });
+
+  $('.header-btn').on('click', () => {
+    $('.menu').addClass('active');
+  });
+
+  $('.close-btn').on('click', () => {
+    $('.menu').removeClass('active');
+  });
 });
